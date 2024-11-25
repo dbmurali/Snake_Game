310431ef30aa26cc888466ec184fc989bd9bbc51
dbmurali <dbmurali1507@gmail.com>
2024-11-25 16:22:38 +0530

    Adding main file

---
+++ main.py
@@ -0,0 +1,49 @@
+from snake import Snake
+from turtle import Screen
+from  food import Food
+from scoreboad import Scoreboad
+
+
+import time
+#SCREEN SETUP
+screen=Screen()
+screen.tracer(0)
+screen.setup(600,600)
+screen.bgcolor("black")
+screen.listen()
+
+#OBJECTS FOR SCORE,SNAKE,FOOD
+score=Scoreboad()
+snake=Snake()
+food=Food()
+
+#KEY CONTROLS
+screen.onkey(snake.left,"Left")
+screen.onkey(snake.right,"Right")
+screen.onkey(snake.up,"Up")
+screen.onkey(snake.down,"Down")
+
+
+
+# LOOPING GAME
+game_on=True
+while game_on:
+    # HEAD POSITION
+    x_pos = snake.head.xcor()
+    y_pos = snake.head.ycor()
+
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+    if snake.head.distance(food)<15:
+       food.refresh()
+       score.increase_score()
+    if x_pos>=280 or x_pos<= -280 or y_pos>=280 or y_pos<= -280:
+        game_on=False
+        score.game_over()
+
+
+
+
+
+screen.exitonclick()
